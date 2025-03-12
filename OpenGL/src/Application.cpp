3a5091b393d050a4b851bb066e319680eb815a23
Vivek <itsvks19@gmail.com>
2025-03-12 12:47:40 +0530

    format code

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -43,14 +43,14 @@ static unsigned int CompileShader(unsigned int type, const std::string& source) 
     unsigned int id = glCreateShader(type);
     const char* src = source.c_str();
     glShaderSource(id, 1, &src, nullptr);
-    glCompileShader(id);
+	glCompileShader(id);
 
     int result;
     glGetShaderiv(id, GL_COMPILE_STATUS, &result);
     if (result == GL_FALSE) {
         int length;
 		glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
-		char* message = (char*)alloca(length * sizeof(char));
+		char* message = (char*)malloc(length * sizeof(char));
 		glGetShaderInfoLog(id, length, &length, message);
 		std::cout << "Failed to compile " << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << " shader!" << std::endl;
 		std::cout << message << std::endl;
